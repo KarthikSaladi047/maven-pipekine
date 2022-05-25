1eda7438d4d670942038c14f34b92722a6c954be
karthik <karthiksaladi@3rmz8z.onmicrosoft.com>
2022-05-25 17:17:00 +0530

    finally the commit

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,7 +3,7 @@
 echo "%%%%%%%%%%%%%%%%%%"
 echo "testing  jar file"
 echo "%%%%%%%%%%%%%%%%%%"
-WORKSPACE=/docker/ansible-jenkins/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven "$@"
 
