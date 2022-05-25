6c3b8929bddd7ab26fe77cf1f0d7b174a7a281bd
karthik <karthiksaladi@3rmz8z.onmicrosoft.com>
2022-05-25 17:32:43 +0530

    finally the commit 32

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,5 +5,6 @@ echo "building jar file"
 echo "%%%%%%%%%%%%%%%%%%"
 WORKSPACE=/docker/ansible-jenkins/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven "$@" 
+docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine:wq! "$@"
+
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,7 +5,7 @@ echo "testing  jar file"
 echo "%%%%%%%%%%%%%%%%%%"
 WORKSPACE=/docker/ansible-jenkins/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven "$@"
+docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
 
 ~                                                                               
 ~                                                                               
