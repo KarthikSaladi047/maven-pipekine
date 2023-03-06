12e9666d647b3cc7a154edc08c3a7f2299a0429b
Karthik Saladi <105864615+KarthikSaladi047@users.noreply.github.com>
2023-03-06 13:11:57 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,6 @@
 echo "%%%%%%%%%%%%%%%%%%"
 echo "building jar file"
 echo "%%%%%%%%%%%%%%%%%%"
-WORKSPACE=/var/jenkins_home/workspace/maven-project
+WORKSPACE=/home/projects/jenkins-volume/workspace/maven-project
 
 docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:latest "$@"
