5d79bc78430e8c29579d74b7e5f7ebe1bf785467
Karthik Saladi <105864615+KarthikSaladi047@users.noreply.github.com>
2023-03-06 13:31:04 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,4 +5,4 @@ echo "testing  jar file"
 echo "%%%%%%%%%%%%%%%%%%"
 WORKSPACE=/home/karthik/projects/jenkins-volume/workspace/maven-project
 
-docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:latest "$@"
