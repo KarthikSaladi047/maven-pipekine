de2a2685954608eab5a8db91d57cfce71049e875
Karthik Saladi <105864615+KarthikSaladi047@users.noreply.github.com>
2023-03-06 12:11:44 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,8 +3,5 @@
 echo "%%%%%%%%%%%%%%%%%%"
 echo "building jar file"
 echo "%%%%%%%%%%%%%%%%%%"
-WORKSPACE=/docker/ansible-jenkins/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v $WORKSPACE/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
-
-
+docker run --rm -v /var/jenkins_home/workspace/maven-project/maven-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
