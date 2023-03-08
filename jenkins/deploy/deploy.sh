305352b2c12a93433b4cac55fa6b6b7cbdf9d2ba
Karthik Saladi <105864615+KarthikSaladi047@users.noreply.github.com>
2023-03-08 14:10:05 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,6 +6,6 @@ echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth karthik@192.168.55.109:/tmp/.auth
 
-scp -i /opt/prod ~/projects/maven-pipeline/jenkins/deploy/publish.sh karthik@192.168.55.109:/tmp/publish.sh
+scp -i /opt/prod ~/workspace/maven-project/jenkins/deploy/publish.sh karthik@192.168.55.109:/tmp/publish.sh
 
 ssh -i /opt/prod karthik@192.168.55.109 "/tmp/publish.sh"
